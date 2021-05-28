09555eb62e784dc187c1632004358440733e1901
muhammadasmar2000 <muhammadasmar2000@gmail.com>
2021-05-28 02:11:50 -0400

    Added transitions between fragments

--- app/src/main/java/com/muhammadasmar/flipforte/Home.java
+++ app/src/main/java/com/muhammadasmar/flipforte/Home.java
@@ -15,10 +15,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
     private MaterialButton signOutButton;
+    private int startingPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        if (savedInstanceState == null) {
+            loadFragment(new SearchFragment(), 0);
+        }
         signOutButton = (MaterialButton) findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,23 +41,51 @@ public class Home extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment = null;
-
+            int newPosition = 0;
             switch(item.getItemId()) {
                 case R.id.search:
                     fragment = new SearchFragment();
+                    newPosition = 1;
                     break;
                 case R.id.record:
                     fragment = new RecordFragment();
+                    newPosition = 2;
                     break;
                 case R.id.library:
                     fragment = new LibraryFragment();
+                    newPosition = 3;
                     break;
                 case R.id.edit:
                     fragment = new EditFragment();
+                    newPosition = 4;
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-            return true;
+            return loadFragment(fragment, newPosition);
         }
     };
+
+    private Boolean loadFragment(Fragment fragment, int newPosition) {
+        if (fragment != null) {
+            if(newPosition == 0) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+            if(startingPosition > newPosition) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.from_left, R.anim.to_right)
+                        .replace(R.id.fragment_container, fragment).commit();
+
+            }
+            if(startingPosition < newPosition) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.from_right, R.anim.to_left)
+                        .replace(R.id.fragment_container, fragment).commit();
+
+            }
+            startingPosition = newPosition;
+            return true;
+        }
+        return false;
+    }
 }
